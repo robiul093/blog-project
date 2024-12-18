08c2117fb46a141b98a037f7d2f2769313b909a6
Robiul@Hasan <robihasan093@gmail.com>
2024-12-18 22:01:43 +0600

    complite project setup

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+
+
+const app: Application = express();
+
+// parser
+app.use(express.json())
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('This is blog project!')
+})
+
+
+
+export default app;
